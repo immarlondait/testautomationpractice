b3d0bf8aa19695e054525d56e61d8d8c07754756
Marlon Dait <immarlondait@gmail.com>
2024-08-15 14:24:25 -0500

    Added dropdown tests

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,36 @@
+import {test, expect} from '@playwright/test'
+
+test('Dropdown num options', async ({page}) =>{
+
+    const numOfOptions = 10
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await expect(page).toHaveURL('https://testautomationpractice.blogspot.com/')
+
+    const options = await page.$$('#country option')
+    
+
+    console.log("Number of options: ", options.length)
+    await expect(options.length).toBe(numOfOptions)
+
+})
+
+test.only('Dropdown order', async ({page}) =>{
+    
+    const countryOrder = ['United States', 'Canada', 'United Kingdom', 
+                          'Germany', 'France', 'Australia', 
+                          'Japan', 'China', 'Brazil', 'India']
+    
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+    await expect(page).toHaveURL('https://testautomationpractice.blogspot.com/')
+
+    const options = await page.$$('#country option')
+    
+    let index = 0
+    for(const country of options){
+        const value = await country.textContent()
+        await expect(await value).toContain(countryOrder[index])
+        index++
+    }
+})
