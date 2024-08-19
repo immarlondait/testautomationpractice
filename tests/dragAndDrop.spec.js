18169288536541e1934f25f84f26f21af61df9fb
Marlon Dait <immarlondait@gmail.com>
2024-08-19 16:13:08 -0500

    added dragAndDrop tests

--- tests/dragAndDrop.spec.js
+++ tests/dragAndDrop.spec.js
@@ -4,13 +4,32 @@ test('Drag and Drop', async ({page}) =>{
 
     await page.goto('https://testautomationpractice.blogspot.com/')
 
+    // validate droppable says "Drop here" by default
+    
+    let dropText = await page.locator("div[id='droppable'] p").textContent()
+
+    await expect(dropText).toBe("Drop here")
+
+    const dragable  = await page.locator('#draggable')
+    const droppable = await page.locator('#droppable')
+    await dragable.hover()
+    await page.mouse.down()
+    await droppable.hover()
+    await page.mouse.up()
+
+    //update dropText
+    dropText = await page.locator('#droppable').textContent()
+
+    // couldn't get exact toBe to work, but contains work. 
+    // to fix: figure out how to make this more specific
+    await expect(dropText).toContain("Dropped!")
 
 
     
 
     
 
-    await page.waitForTimeout(2000) 
+    //await page.waitForTimeout(2000) 
     
 })
 
