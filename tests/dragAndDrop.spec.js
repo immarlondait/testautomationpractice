ec184a3dbde0ef977f9ab8f333d3cb24c3c49c71
Marlon Dait <immarlondait@gmail.com>
2024-08-19 14:01:13 -0500

    Setting up Drag and Drop test

---
+++ tests/dragAndDrop.spec.js
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test'
+
+test('Drag and Drop', async ({page}) =>{
+
+    await page.goto('https://testautomationpractice.blogspot.com/')
+
+
+
+    
+
+    
+
+    await page.waitForTimeout(2000) 
+    
+})
+
