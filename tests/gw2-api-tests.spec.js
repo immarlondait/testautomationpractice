b161002a706568f302187aaa457a7a0450f3126e
Marlon Dait <immarlondait@gmail.com>
2024-08-26 14:33:58 -0500

    Updated gw2 character api

--- tests/gw2-api-tests.spec.js
+++ tests/gw2-api-tests.spec.js
@@ -7,20 +7,45 @@ test.describe.configure({mode: 'serial'})
 const apiKey = process.env.access_token
 
 
-test("Status test - 200 success", async ({request}) => {
+test("Character API - Status test - 200 success", async ({request}) => {
+
+    const response = await request.get(`https://api.guildwars2.com/v2/characters?access_token=${apiKey}`)
+    await expect(response.status()).toBe(200)
+
+    //const responseBody = JSON.parse(await response.text())
+
+    //console.log(responseBody)
+
+})
+
+test("Character API - Status test - 401 fail", async ({request}) => {
+
+    const response = await request.get(`https://api.guildwars2.com/v2/characters?access_token=bad_key`)
+    await expect(response.status()).toBe(401)
+    
+
+    const responseBody = JSON.parse(await response.text())
+    await expect(responseBody.text).toBe("Invalid access token")
+
+
+})
+
+test("Character API - Character List", async ({request}) => {
 
     const response = await request.get(`https://api.guildwars2.com/v2/characters?access_token=${apiKey}`)
     await expect(response.status()).toBe(200)
 
     const responseBody = JSON.parse(await response.text())
+    await expect(responseBody).toContain("Yokai")
+    await expect(responseBody).toContain("Poufsouffle")
+    await expect(responseBody).toContain("Rinne Snow")
+    await expect(responseBody).toContain("Yokai The Engineer")
+    await expect(responseBody).toContain("Brynndalynn")
+    await expect(responseBody).toContain("Yokai The Warrior")
+    await expect(responseBody).toContain("Ein The Fox")
+    await expect(responseBody).toContain("Nocte Cereus")
+    await expect(responseBody).toContain("Yokai Bank")
+    await expect(responseBody).toContain("Yokai The Ranger")
 
-    console.log(responseBody)
-
-})
-
-test("API - Status test - 404 not found", async ({request}) => {
-
-    const response = await request.get('https://reqres.in/api/users/asdf')
-    await expect(response.status()).toBe(404)
 
 })
