97419688d509cca79b250046da5d9dba3ab5e770
Marlon Dait <immarlondait@gmail.com>
2024-09-04 17:05:28 -0500

    Updated doubleclick test

--- tests/doubleclick.spec.js
+++ tests/doubleclick.spec.js
@@ -7,20 +7,25 @@ test('Double Click', async ({page}) =>{
     const f1 = await page.locator('#field1')
     const f2 = await page.locator('#field2')
 
+    // first we check the default state of the page
+    await expect(await f1).toHaveValue("Hello World!")
+    await expect(await f2).toBeEmpty()
+
     
+    // change the value in field1
     await f1.fill('Marlon Dait')
-    
-    //await expect(await page.locator('#name')).toHaveValue('Marlon Dait')
     await expect(await f1).toHaveValue("Marlon Dait")
     
 
+    // test a single click
     await page.locator("//button[normalize-space()='Copy Text']").click()
+    // Since this is a single click, field2 should be empty
+    await expect(await f2).toBeEmpty()  
 
-    await expect(await f2).toBeEmpty()  // Since this is a single click, field2 should be empty
 
-
+    // test a double click
     await page.locator("//button[normalize-space()='Copy Text']").dblclick()
-
+    // This should now copy the value in field1
     await expect(await f2).toHaveValue("Marlon Dait")
     
 
