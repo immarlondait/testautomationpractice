ea2af40a12f71bd77b206303db282c0760c5beaa
Marlon Dait <immarlondait@gmail.com>
2024-09-08 12:08:07 -0500

    gw2 char api added specializations test

--- tests/gw2-api-character.spec.js
+++ tests/gw2-api-character.spec.js
@@ -332,3 +332,22 @@ test("Character API - Skills", async ({request}) => {
 
 })
 // endregion
+
+// region Specializations
+test("Character API - Specializations", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/characters/${myChar}/specializations?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+    
+
+    const responseBody = JSON.parse(await response.text())
+    // await console.log(responseBody)
+
+    //test active
+    await expect(responseBody).toBeTruthy()
+    
+
+
+})
+// endregion
