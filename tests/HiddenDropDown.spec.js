de91f68e01cd487fbe97c4601113aa379f489450
Marlon Dait <immarlondait@gmail.com>
2024-09-08 12:09:23 -0500

    Added hidden dropdown test

---
+++ tests/HiddenDropDown.spec.js
@@ -0,0 +1,34 @@
+const {test, expect} = require('@playwright/test')
+
+test('Hidden options dropdown', async ({page}) =>{
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+    // We use SelectorsHub Debug feature, click and then open dropdown within 5 seconds of pressing Debug
+
+    // Navigate to page
+    await page.locator("[name='username']").fill('Admin')
+    await page.locator("[name='password']").fill('admin123')
+    await page.locator("[type='submit']").click()
+    await page.locator("//span[normalize-space()='PIM']").click()
+
+
+    // Subunit locator
+    await page.locator("//div[7]//div[1]//div[2]//div[1]//div[1]//div[1]").click()
+
+    // waiting for options
+    await page.waitForTimeout(2000)
+    
+    const options = await page.$$("//div[@role='listbox']//span")
+
+    for(let option of options){
+        const subUnit = await option.textContent()
+        // console.log("Sub Unit: ", subUnit)
+        if(subUnit.includes("Quality Assurance")){
+            await option.click()
+            break
+        }
+    }
+    await page.waitForTimeout(2000) //pausing execution, give you time to get eyes on it
+    
+})
