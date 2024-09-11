ae4ffd5d8f9cb38ea4e5b7f85533e5ca2e0c424f
Marlon Dait <immarlondait@gmail.com>
2024-09-11 13:24:00 -0500

    Fixed HiddenDropDown test

--- tests/HiddenDropDown.spec.js
+++ tests/HiddenDropDown.spec.js
@@ -14,7 +14,8 @@ test('Hidden options dropdown', async ({page}) =>{
 
 
     // Subunit locator
-    await page.locator("div:nth-child(7) div:nth-child(1) div:nth-child(2) div:nth-child(1) div:nth-child(1) div:nth-child(1)").click()
+    // Flaky - sometimes page comes up in different language
+    await page.locator("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)").click()
 
     // waiting for options
     await page.waitForTimeout(2000)
