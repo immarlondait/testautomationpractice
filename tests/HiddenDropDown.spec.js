183d1654f63d1cef78f156476fbfd5863fd0eb63
Marlon Dait <immarlondait@gmail.com>
2024-09-12 01:27:53 -0500

    Fixed HiddenDropDown to correct locator

--- tests/HiddenDropDown.spec.js
+++ tests/HiddenDropDown.spec.js
@@ -15,7 +15,7 @@ test('Hidden options dropdown', async ({page}) =>{
 
     // Subunit locator
     // Flaky - sometimes page comes up in different language
-    await page.locator("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > aside:nth-child(1) > nav:nth-child(1) > div:nth-child(2) > ul:nth-child(2) > li:nth-child(2) > a:nth-child(1)").click()
+    await page.locator("div:nth-child(7) div:nth-child(1) div:nth-child(2) div:nth-child(1) div:nth-child(1) div:nth-child(1)").click()
 
     // waiting for options
     await page.waitForTimeout(2000)
