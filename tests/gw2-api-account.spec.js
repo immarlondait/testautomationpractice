5018582ef9f25471cca4c660b37888395ed8ded6
Marlon Dait <immarlondait@gmail.com>
2024-09-14 00:55:49 -0500

    Added gw2 api account bank test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -35,3 +35,16 @@ test("Account  API - Achievements ", async ({request}) => {
 })
 // endregion
 
+// region Bank
+test("Account  API - Bank ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/bank?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
+
