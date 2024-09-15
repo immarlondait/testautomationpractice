cd96222b7f8eb835ebb4c93e043395269a1ba894
Marlon Dait <immarlondait@gmail.com>
2024-09-15 10:52:42 -0500

    updated automation POM

--- pageObjects/AutomationPage.js
+++ pageObjects/AutomationPage.js
@@ -2,8 +2,24 @@
 class AutomationPage {
     constructor(page) {
       this.page = page;
+
       this.nameField = '#name'  // Locator for name input field
       this.emailField = '#email' // Locator for email field
+      this.phoneField = '#phone' // Locator for phone field
+      this.addressField = '#textarea' // Locator for Addresse field
+
+      this.maleRadio = '#male' // Male Radio button
+      this.femaleRadio = '#female' //Female Radio button
+
+      this.sundayCheckbox = '#sunday'
+      this.mondayCheckbox = '#monday'
+      this.tuesdayCheckbox = '#tuesday'
+      this.wednesdayCheckbox = '#wednesday'
+      this.thursdayCheckbox = '#thursday'
+      this.fridayCheckbox = '#friday'
+      this.saturdayCheckbox = '#saturday'
+
+      
 
     }
   
