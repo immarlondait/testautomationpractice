a78b165030446df25a3b967e488cce9169b51179
Marlon Dait <immarlondait@gmail.com>
2024-09-15 20:49:18 -0500

    Updated AutomationPage POM

--- pageObjects/AutomationPage.js
+++ pageObjects/AutomationPage.js
@@ -3,15 +3,15 @@ class AutomationPage {
     constructor(page) {
       this.page = page;
 
-      this.nameField = '#name'  // Locator for name input field
-      this.emailField = '#email' // Locator for email field
-      this.phoneField = '#phone' // Locator for phone field
-      this.addressField = '#textarea' // Locator for Addresse field
+      this.nameField = '#name'                    // Locator for name input field
+      this.emailField = '#email'                  // Locator for email field
+      this.phoneField = '#phone'                  // Locator for phone field
+      this.addressField = '#textarea'             // Locator for Addresse field
 
-      this.maleRadio = '#male' // Male Radio button
-      this.femaleRadio = '#female' //Female Radio button
+      this.maleRadio = '#male'                    // Male Radio button
+      this.femaleRadio = '#female'                //Female Radio button
 
-      this.sundayCheckbox = '#sunday' // Days of the week checkbox
+      this.sundayCheckbox = '#sunday'             // Days of the week checkbox
       this.mondayCheckbox = '#monday'
       this.tuesdayCheckbox = '#tuesday'
       this.wednesdayCheckbox = '#wednesday'
@@ -19,11 +19,17 @@ class AutomationPage {
       this.fridayCheckbox = '#friday'
       this.saturdayCheckbox = '#saturday'
 
-      this.countryDropdown = '#country' // Country dropdown
+      this.countryDropdown = '#country'           // Country dropdown
+
+      this.colorRed = "option[value='red']"       // Color - Red
+      this.colorBlue = "option[value='blue']"     // Color - Blue
+      this.colorGreen = "option[value='green']"   // Color - Green
+      this.colorYellow = "option[value='yellow']" // Color - Yellow
 
 
     }
   
+    // region Text Input Fields
     // Action to fill in name
     async enterName(name) {
       await this.page.fill(this.nameField, name);
@@ -43,6 +49,8 @@ class AutomationPage {
     async enterPassword(address) {
       await this.page.fill(this.addressField, address);
     }
+    // endregion
+
   
 
   }
