5464f63c9539bc207a798bdef68df3a3de07e026
Marlon Dait <immarlondait@gmail.com>
2024-11-23 18:53:05 -0600

    Added api homestead status test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -177,3 +177,17 @@ test("Account  API - Home/nodes ", async ({request}) => {
 
 })
 // endregion
+
+
+// region Homestead
+test("Account  API - Homestead ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/homestead?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
