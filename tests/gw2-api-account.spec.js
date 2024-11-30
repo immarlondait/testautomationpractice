a702a0b648ab93035eaf085dfe12436f0fd83e24
Marlon Dait <immarlondait@gmail.com>
2024-11-30 14:49:11 -0600

    Added /account/mailcarriers api status check

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -269,3 +269,16 @@ test("Account  API - Luck ", async ({request}) => {
 
 })
 // endregion
+
+// region Mail Carriers
+test("Account  API - Mail Carriers ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/mailcarriers?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
