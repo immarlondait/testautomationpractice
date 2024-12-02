5ba8185b3c856329473fe0c27c3763f8cd9815c1
Marlon Dait <immarlondait@gmail.com>
2024-12-02 01:25:05 -0600

    Added /account/masteries api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -295,3 +295,16 @@ test("Account  API - Map Chests ", async ({request}) => {
 
 })
 // endregion
+
+// region Masteries
+test("Account  API - Masteries ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/masteries?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
