e9d8372b7c842a2de01c06e1256acb6316752242
Marlon Dait <immarlondait@gmail.com>
2024-12-08 01:48:19 -0600

    Added /account/mounts/skins api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -360,3 +360,16 @@ test("Account  API - Mounts ", async ({request}) => {
 
 })
 // endregion
+
+// region Mounts/Skins
+test("Account  API - Mounts/Skins ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/mounts/skins?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
