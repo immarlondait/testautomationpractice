ad33b7c2c466c7f301b6fe7c90d0718bef6c7fa1
Marlon Dait <immarlondait@gmail.com>
2024-12-09 02:15:18 -0600

    Added /account/mounts/types success api test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -373,3 +373,16 @@ test("Account  API - Mounts/Skins ", async ({request}) => {
 
 })
 // endregion
+
+// region Mounts/Types
+test("Account  API - Mounts/Types ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/mounts/types?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
