f9a393912713e8712c904e91fd4cac610e7f4a21
Marlon Dait <immarlondait@gmail.com>
2024-12-15 09:21:24 -0600

    Added /account/recipes api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -451,3 +451,16 @@ test("Account  API - Raids ", async ({request}) => {
 
 })
 // endregion
+
+// region Recipes
+test("Account  API - Recipes ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/pvp/recipes?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
