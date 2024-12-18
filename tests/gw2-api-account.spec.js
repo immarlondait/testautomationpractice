9690c5812d87d51de990f7762e9c23ef0ca90666
Marlon Dait <immarlondait@gmail.com>
2024-12-18 00:32:57 -0600

    Added /account/wallet api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -503,3 +503,16 @@ test("Account  API - Titles ", async ({request}) => {
 
 })
 // endregion
+
+// region Wallet
+test("Account  API - Wallet ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/pvp/wallet?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
