83d33f66667769cad315df7e759b6b385c4b6602
Marlon Dait <immarlondait@gmail.com>
2024-12-21 10:25:50 -0600

    Added /account/wizardsvault/listings api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -545,4 +545,18 @@ test("Account  API - Wizards Vault/listings ", async ({request}) => {
 
 
 })
+
+
+test("Account  API - Wizards Vault/special ", async ({request}) => {
+
+    // returns 404/503 errors if requested, have to go direct to sub-endpoints
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/pvp/wizardsvault/special?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
 // endregion
