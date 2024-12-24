3565fbea5296998d7ef12fa634254ba9d52c19aa
Marlon Dait <immarlondait@gmail.com>
2024-12-24 01:42:55 -0600

    Added /account/achievements api success test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -587,3 +587,16 @@ test("Account  API - World Bosses ", async ({request}) => {
 
 })
 // endregion
+
+// region Achievements
+test("Account  API - Achievements ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/pvp/achievements?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
