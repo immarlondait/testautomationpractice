3c26cd12346e85414bf010afe0488afdfe7ab5eb
Marlon Dait <immarlondait@gmail.com>
2024-12-25 06:29:05 -0600

    Added /account/achievements/categories success test. Merry Christmas!

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -600,3 +600,16 @@ test("Account  API - Achievements ", async ({request}) => {
 
 })
 // endregion
+
+// region Achievements/Categories
+test("Account  API - Achievements/Categories ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/account/pvp/achievements/categories?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+// endregion
