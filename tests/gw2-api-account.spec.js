8fd72376a321cb7f946b8a1415bd69a405bfb3ef
Marlon Dait <immarlondait@gmail.com>
2025-01-09 00:20:03 -0600

    Added /commerce api error test

--- tests/gw2-api-account.spec.js
+++ tests/gw2-api-account.spec.js
@@ -729,3 +729,18 @@ test("Colors", async ({request}) => {
 // endregion
 
 
+// region Colors
+test("Commerce 404", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/commerce?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(404) //expected to be 404 or 503 when requested directly
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+// endregion
+
+
