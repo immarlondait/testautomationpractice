e7c0e4f9b33606eb0c2c67c466ec5b7bb2ee52fc
Marlon Dait <immarlondait@gmail.com>
2025-01-13 00:33:49 -0600

    Added gw2 api daily rewards test

---
+++ tests/gw2-api-daily-rewards.spec.js
@@ -0,0 +1,19 @@
+import {test, expect} from "@playwright/test"
+require('dotenv').config()
+
+
+const API_KEY = process.env.API_KEY
+
+
+
+
+test("dailycrafting - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/dailycrafting?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
