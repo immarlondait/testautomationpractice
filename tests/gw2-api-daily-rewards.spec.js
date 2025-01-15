fa82a752e53c993f08b08572bde0a5b10698abf0
Marlon Dait <immarlondait@gmail.com>
2025-01-15 00:15:26 -0600

    Added worldbosses status check test

--- tests/gw2-api-daily-rewards.spec.js
+++ tests/gw2-api-daily-rewards.spec.js
@@ -28,3 +28,14 @@ test("mapchests - Status check ", async ({request}) => {
 
 
 })
+
+test("worldbosses - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/worldbosses?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
