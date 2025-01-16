92ebca1c04af8d8772d4547d200a5d1071775529
Marlon Dait <immarlondait@gmail.com>
2025-01-16 00:19:20 -0600

    Added game mechanics api test class and jadebots status check

---
+++ tests/gw2-api-game-mechanics.js
@@ -0,0 +1,20 @@
+import {test, expect} from "@playwright/test"
+require('dotenv').config()
+
+
+const API_KEY = process.env.API_KEY
+
+
+
+
+test("jadebots - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/jadebots?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
