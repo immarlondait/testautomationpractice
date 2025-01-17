d202156e80f1e0ce5fd65dbd37844e79d5f89d6d
Marlon Dait <immarlondait@gmail.com>
2025-01-17 09:24:00 -0600

    Added legendaryarmory api success test

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -18,3 +18,13 @@ test("jadebots - Status check ", async ({request}) => {
 
 })
 
+test("legendaryarmory - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/legendaryarmory?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
