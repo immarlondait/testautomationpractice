c05c2f008f0969495c817c1ec3f5ba7f6512992f
Marlon Dait <immarlondait@gmail.com>
2025-01-20 00:19:54 -0600

    Added game mechanics mounts api success test

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -39,3 +39,14 @@ test("masteries - Status check ", async ({request}) => {
 
 
 })
+
+test("mounts - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/mounts?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
