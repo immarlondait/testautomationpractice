eadfb0b94f16f5039f8ca2fc6eb7c1a5a5911a8b
Marlon Dait <immarlondait@gmail.com>
2025-01-23 00:44:42 -0600

    Added /mounts/types success api check

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -51,10 +51,21 @@ test("mounts - Status check ", async ({request}) => {
 
 })
 
-test("mounts/skins -  Status  check ", async ({request}) => {
+test("mounts/skins - Status check ", async ({request}) => {
 
 
     const response = await request.get(`https://api.guildwars2.com/v2/mounts/skins?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+test("mounts/types - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/mounts/types?access_token=${API_KEY}`)
     await expect(response.status()).toBe(200)
 
     const responseBody = JSON.parse(await response.text())
