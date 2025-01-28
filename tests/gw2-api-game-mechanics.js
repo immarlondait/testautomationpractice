ccae347b7aea5f6ca247472a077d5725f1db9759
Marlon Dait <immarlondait@gmail.com>
2025-01-28 00:07:10 -0600

    Added /v2/races api success test

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -105,3 +105,14 @@ test("professions - Status check ", async ({request}) => {
 
 
 })
+
+test("races - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/races?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
