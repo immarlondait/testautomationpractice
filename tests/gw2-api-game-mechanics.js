ececddedf783d0d2225ff0a10b98db4827815c2b
Marlon Dait <immarlondait@gmail.com>
2025-01-29 17:37:12 -0600

    Added /v2/specializations success api check

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -116,3 +116,14 @@ test("races - Status check ", async ({request}) => {
 
 
 })
+
+test("specializations - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/specializations?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
