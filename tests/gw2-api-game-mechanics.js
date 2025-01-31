c19e980da5997a718af2deb41f6984fe2a86e825
Marlon Dait <immarlondait@gmail.com>
2025-01-31 00:19:54 -0600

    Added /v2/skills api success check

--- tests/gw2-api-game-mechanics.js
+++ tests/gw2-api-game-mechanics.js
@@ -138,3 +138,14 @@ test("skiffs - Status check ", async ({request}) => {
 
 
 })
+
+test("skills - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/skills?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
