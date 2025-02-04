03dea952b8c50cfe1102078e4f3202a711453349
Marlon Dait <immarlondait@gmail.com>
2025-02-04 10:21:06 -0600

    Added /v2/guild/permissions api success check

--- tests/gw2-api-guild.spec.js
+++ tests/gw2-api-guild.spec.js
@@ -17,3 +17,14 @@ test("emblem - Status check ", async ({request}) => {
 
 
 })
+
+test("guild/permissions - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/guild/permissions?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
