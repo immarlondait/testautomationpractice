1055c1825f721053ab7efadc06e0f7df71f0b704
Marlon Dait <immarlondait@gmail.com>
2025-02-06 00:18:57 -0600

    Added /v2/guild/upgrades api success check

--- tests/gw2-api-guild.spec.js
+++ tests/gw2-api-guild.spec.js
@@ -39,3 +39,14 @@ test("guild/search - Status check ", async ({request}) => {
 
 
 })
+
+test("guild/upgrades - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/guild/upgrades?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
