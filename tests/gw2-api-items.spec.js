b23da19cad9f1371afebcc7c5ea99abd022c3a7f
Marlon Dait <immarlondait@gmail.com>
2025-02-16 00:13:20 -0600

    Added /v2/pvp/amulets api success check

--- tests/gw2-api-items.spec.js
+++ tests/gw2-api-items.spec.js
@@ -55,3 +55,14 @@ test("materials - Status check ", async ({request}) => {
 
 })
 
+test("pvp/amulets - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/pvp/amulets?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
