07d804ee6a14a3131c4be7c29237af2f1dc3b0d7
Marlon Dait <immarlondait@gmail.com>
2025-02-20 00:10:58 -0600

    Added gw2-api.map-information.spec.js

---
+++ tests/gw2-api-map-information.spec.js
@@ -0,0 +1,21 @@
+import {test, expect} from "@playwright/test"
+require('dotenv').config()
+
+
+const API_KEY = process.env.API_KEY
+
+
+
+
+test("continents - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/continents?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
