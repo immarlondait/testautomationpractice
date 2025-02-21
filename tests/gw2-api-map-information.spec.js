95a009e03ef86be800ff17a0cd94e9cfa94ef1c7
Marlon Dait <immarlondait@gmail.com>
2025-02-21 00:03:17 -0600

    Added /v2/maps api success check

--- tests/gw2-api-map-information.spec.js
+++ tests/gw2-api-map-information.spec.js
@@ -19,3 +19,15 @@ test("continents - Status check ", async ({request}) => {
 })
 
 
+test("maps - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/maps?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
