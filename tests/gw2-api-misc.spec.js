03579696d549f22069db0a321a06ae8c778bd048
Marlon Dait <immarlondait@gmail.com>
2025-02-25 00:10:46 -0600

    Added /v2/dungeons api success check

--- tests/gw2-api-misc.spec.js
+++ tests/gw2-api-misc.spec.js
@@ -42,3 +42,14 @@ test("currencies - Status check ", async ({request}) => {
 
 })
 
+test("dungeons - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/dungeons?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
