2fce22ec23edd8aed48db51b78a4254e4a47acc7
Marlon Dait <immarlondait@gmail.com>
2025-03-04 00:21:22 -0600

    Added /v2/backstory/answers api success check

---
+++ tests/gw2-api-story.spec.js
@@ -0,0 +1,21 @@
+import {test, expect} from "@playwright/test"
+require('dotenv').config()
+
+
+const API_KEY = process.env.API_KEY
+
+
+
+
+test("backstory/answers - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/backstory/answers?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
