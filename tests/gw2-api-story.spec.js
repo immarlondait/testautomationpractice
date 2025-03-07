1e75e6ad284ee055246925dd668cc6ecd442a3b3
Marlon Dait <immarlondait@gmail.com>
2025-03-07 00:20:41 -0600

    Added /v2/stories/seasons api success check

--- tests/gw2-api-story.spec.js
+++ tests/gw2-api-story.spec.js
@@ -43,3 +43,15 @@ test("stories - Status check ", async ({request}) => {
 })
 
 
+test("stories/seasons - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/stories/seasons?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
