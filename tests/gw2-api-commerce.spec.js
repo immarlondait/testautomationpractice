5486d5acdab1c2ace568c7b001c0a916d9bf92bc
Marlon Dait <immarlondait@gmail.com>
2025-03-14 00:09:14 -0600

    Added /v2/commerce/exchange/coins api success check

--- tests/gw2-api-commerce.spec.js
+++ tests/gw2-api-commerce.spec.js
@@ -31,3 +31,15 @@ test("/commerce/exchange - Status check ", async ({request}) => {
 })
 
 
+test("/commerce/exchange/coins - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/backstory/commerce/exchange/coins?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
