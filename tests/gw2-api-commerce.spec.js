1a02e93f5dba9a6af24b841bc52a52e401ef16b3
Marlon Dait <immarlondait@gmail.com>
2025-03-16 00:58:30 -0600

    Added /v2/commerce/listings api success check

--- tests/gw2-api-commerce.spec.js
+++ tests/gw2-api-commerce.spec.js
@@ -55,3 +55,15 @@ test("/commerce/exchange/gems - Status check ", async ({request}) => {
 })
 
 
+test("/commerce/listings - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/backstory/commerce/listings?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
