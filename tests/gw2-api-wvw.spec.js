0c92427ffa792bc7ec86643ddf3a21f4158bea4d
Marlon Dait <immarlondait@gmail.com>
2025-03-26 08:57:08 -0600

    Added /v2/wvw/guilds api success check

--- tests/gw2-api-wvw.spec.js
+++ tests/gw2-api-wvw.spec.js
@@ -31,3 +31,15 @@ test("wvw/abilities - Status check ", async ({request}) => {
 })
 
 
+test("wvw/guilds - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v2/wvw/guilds?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
