89ef36446413eff7bce43f3a43c8c20ab84e93e5
Marlon Dait <immarlondait@gmail.com>
2025-04-05 00:11:27 -0600

    Added /v1/guild_details api success check

--- tests/gw2-api-v1.spec.js
+++ tests/gw2-api-v1.spec.js
@@ -69,3 +69,16 @@ test("event_details - Status check ", async ({request}) => {
 
 
 
+test("guild_details - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v1/guild_details?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
+
