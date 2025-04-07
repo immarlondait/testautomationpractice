c50825de97e7061cedf6b568e80ac15c4ec3d3bb
Marlon Dait <immarlondait@gmail.com>
2025-04-07 14:01:16 -0600

    Added /v1/item_details api success check

--- tests/gw2-api-v1.spec.js
+++ tests/gw2-api-v1.spec.js
@@ -95,3 +95,16 @@ test("items - Status check ", async ({request}) => {
 
 
 
+test("item_details - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v1/item_details?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
+
