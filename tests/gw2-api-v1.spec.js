d7411a1194156230d04eeb1bad7b7d92b8eb1d85
Marlon Dait <immarlondait@gmail.com>
2025-04-11 20:21:24 -0600

    Added /v1/skin_details api success check

--- tests/gw2-api-v1.spec.js
+++ tests/gw2-api-v1.spec.js
@@ -145,6 +145,19 @@ test("skins - Status check ", async ({request}) => {
 
 })
 
+test("skin_details - Status check ", async ({request}) => {
+
+
+    const response = await request.get(`https://api.guildwars2.com/v1/skin_details?access_token=${API_KEY}`)
+    await expect(response.status()).toBe(200)
+
+    const responseBody = JSON.parse(await response.text())
+
+
+})
+
+
+
 
 
 
